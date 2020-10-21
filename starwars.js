b26145cf5dbf8d9081ccc76f9124b7dc88e72e0c
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-10-21 15:39:19 -0400

    [SW/Iphone] Added starwars.js and iphone.js

---
+++ starwars.js
@@ -0,0 +1,10 @@
+const mando = {
+    image: " path to where image is located",
+    length: "1:00",
+    title: "Special Look | The Mandolorian | Disney+",
+    username: "Star Wars",
+    views: "1 Million",
+    uploaded: 1 ,
+}
+
+console.log(mando.title)
